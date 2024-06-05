752c9dc4c8d29cf3d922beda03e67d700dffd530
Victor Cizek <victorjcizek@gmail.com>
2024-06-05 02:18:15 -0400

    update scale from resize to envelope

--- moonbugs.js
+++ moonbugs.js
@@ -803,7 +803,7 @@ class Level3 extends Phaser.Scene {
 const config = {
     type: Phaser.AUTO,
     scale: {
-        mode: Phaser.Scale.STRETCH,
+        mode: Phaser.Scale.ENVELOPE,
         autoCenter: Phaser.Scale.CENTER_BOTH,
         width: 800,
         height: 600
