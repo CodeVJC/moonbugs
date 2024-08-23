aea99046c5260d28192612ad5958541a39fb1f21
CodeVJC <victorjcizek@gmail.com>
2024-08-23 16:45:40 -0400

    load fonts ahead of game start

--- src/index.js
+++ src/index.js
@@ -1,21 +1,28 @@
 import Preload from "./Preload.js";
 
-// create game config and game
-const config = {
-    type: Phaser.AUTO,
-    scale: {
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH,
-        width: 800,
-        height: 600
+// Load WebFontLoader
+WebFont.load({
+    google: {
+        families: ['Concert One', 'Rubik Moonrocks']
     },
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 0 },
-        }
-    },
-    scene: [ Preload ],
-};
-
-new Phaser.Game(config);
+    active: function() {
+        // create game config and game
+        const config = {
+            type: Phaser.AUTO,
+            scale: {
+                mode: Phaser.Scale.FIT,
+                autoCenter: Phaser.Scale.CENTER_BOTH,
+                width: 800,
+                height: 600
+            },
+            physics: {
+                default: 'arcade',
+                arcade: {
+                    gravity: { y: 0 },
+                }
+            },
+            scene: [ Preload ],
+        };
+        new Phaser.Game(config);
+    }
+});
