df36d944dd276bdd2b0ad69a4eec76318f6d6412
Victor Cizek <victorjcizek@gmail.com>
2024-08-24 04:39:38 -0400

    update config

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,15 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
 import { getFirestore, deleteDoc, addDoc, getDocs, collection, orderBy, limit, query, doc } from 'https://www.gstatic.com/firebasejs/10.13/firebase-firestore.js'
-//import firebaseConfig from "../config.js";
-
-const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID
-};
+import firebaseConfig from "../config.js";
 
 const app = initializeApp(firebaseConfig);
 
