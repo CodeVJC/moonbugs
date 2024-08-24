c77ffcc02869d36c35827a759fd839bb425ac2e8
Victor Cizek <victorjcizek@gmail.com>
2024-08-24 03:55:09 -0400

    update config and index files

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,8 +1,18 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
 import { getFirestore, deleteDoc, addDoc, getDocs, collection, orderBy, limit, query, doc } from 'https://www.gstatic.com/firebasejs/10.13/firebase-firestore.js'
-import firebaseConfig from "../config.js";
+//import firebaseConfig from "../config.js";
+
+const firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID
+};
 
 const app = initializeApp(firebaseConfig);
+
 const db = getFirestore(app);
 const leadersRef = collection(db, "leaderboard");
 
--- src/index.js
+++ src/index.js
@@ -1,28 +1,31 @@
 import Preload from "./Preload.js";
+const allowedUrl = 'https://codevjc.github.io/moonbugs/';
 
-// Load WebFontLoader
-WebFont.load({
-    google: {
-        families: ['Concert One', 'Rubik Moonrocks']
-    },
-    active: function() {
-        // create game config and game
-        const config = {
-            type: Phaser.AUTO,
-            scale: {
-                mode: Phaser.Scale.FIT,
-                autoCenter: Phaser.Scale.CENTER_BOTH,
-                width: 800,
-                height: 600
-            },
-            physics: {
-                default: 'arcade',
-                arcade: {
-                    gravity: { y: 0 },
-                }
-            },
-            scene: [ Preload ],
-        };
-        new Phaser.Game(config);
-    }
-});
+if (window.location.href === allowedUrl) {
+    // Load WebFontLoader
+    WebFont.load({
+        google: {
+            families: ['Concert One', 'Rubik Moonrocks']
+        },
+        active: function() {
+            // create game config and game
+            const config = {
+                type: Phaser.AUTO,
+                scale: {
+                    mode: Phaser.Scale.FIT,
+                    autoCenter: Phaser.Scale.CENTER_BOTH,
+                    width: 800,
+                    height: 600
+                },
+                physics: {
+                    default: 'arcade',
+                    arcade: {
+                        gravity: { y: 0 },
+                    }
+                },
+                scene: [ Preload ],
+            };
+            new Phaser.Game(config);
+        }
+    });
+}
