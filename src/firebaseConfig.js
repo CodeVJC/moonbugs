37921fe9855d0f4a387e084155969ac576490739
Victor Cizek <victorjcizek@gmail.com>
2024-09-10 07:42:08 -0400

    update config files

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,8 +1,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
 import { getFirestore, deleteDoc, addDoc, getDocs, collection, orderBy, limit, query, doc } from 'https://www.gstatic.com/firebasejs/10.13/firebase-firestore.js'
-import firebaseConfig from "../config.js";
+import config from "../config.js";
 
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(config);
 
 const db = getFirestore(app);
 const leadersRef = collection(db, "leaderboard");
